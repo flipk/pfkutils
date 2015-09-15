2d4ae1f3c0e49055058e1990db605ea40ecd8c02
Phillip F Knaack <pknaack1@gmail.com>
2015-09-14 23:25:43 -0500

    background and script cleanups

--- scripts/set-background.sh
+++ scripts/set-background.sh
@@ -3,21 +3,18 @@
 # default bg if no image file can be found.
 xsetroot -solid \#102030
 
-bg="none"
-if [ -f $HOME/tmp/grid3.png ] ; then
-    bg=$HOME/tmp/grid3.png
-elif [ -f $HOME/Dropbox/Public/pics/grid3.png ] ; then
-    bg=$HOME/Dropbox/Public/pics/grid3.png
-# more cases for other platforms go here
-fi
-
+bg=$HOME/.background.png
 # one of the following will succeed and the others
 # will make an error msg. redirect the errors to null
-
-if [ $bg != "none" ] ; then
+if [ -f $bg ] ; then
     exec >/dev/null 2>/dev/null
-    xsri --tile=$bg
+    # if feh is installed, use that.
     feh --bg-tile $bg
+    # if xsri is installed, use that.
+    xsri --tile=$bg
+    # maybe 'display' is installed? gross.
+    rootwin=`xdpyinfo | awk '/root window id/ { print $4 }'`
+    display -window $rootwin $bg
 fi
 
 if [ x$1 = xwait ] ; then
