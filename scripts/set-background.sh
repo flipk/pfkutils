3c630f52bfe03d6931158ad1affacee99c666439
Phillip F Knaack <pknaack1@gmail.com>
2016-03-23 21:10:46 -0500

    various fluxbox and background-setting enhancements

--- scripts/set-background.sh
+++ scripts/set-background.sh
@@ -1,21 +1,23 @@
 #!/bin/sh
 
-# default bg if no image file can be found.
-xsetroot -solid \#102030
 
 bg=$HOME/.background.png
+found=0
 # one of the following will succeed and the others
 # will make an error msg. redirect the errors to null
 if [ -f $bg ] ; then
     exec >/dev/null 2>/dev/null
     # if feh is installed, use that.
-    feh --bg-tile $bg
+    feh --bg-tile $bg && found=1
     # if xsri is installed, use that.
-    xsri --tile=$bg
+    [ $found == 0 ] && xsri --tile=$bg && found=1
     # maybe 'display' is installed? gross.
     rootwin=`xdpyinfo | awk '/root window id/ { print $4 }'`
-    display -window $rootwin $bg
+    [ $found == 0 ] && display -window $rootwin $bg && found=1
 fi
+
+# default bg if no image file or no program can be found.
+[ $found == 0 ] && xsetroot -solid \#102030
 
 if [ x$1 = xwait ] ; then
     sleep 3600&
