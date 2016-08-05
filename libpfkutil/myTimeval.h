1efb0d59edab6696bc922a350c953c13e99ac739
Phillip F Knaack <pknaack1@gmail.com>
2016-08-04 20:46:11 -0500

    added myTimespec and operator= which converts between ts and tv

--- libpfkutil/myTimeval.h
+++ libpfkutil/myTimeval.h
@@ -11,8 +11,18 @@
 struct myTimeval : public timeval
 {
     myTimeval(void) { tv_sec = 0; tv_usec = 0; }
-    myTimeval(typeof(tv_sec) s, typeof(tv_usec) u) { set(s,u); }
-    void set(typeof(tv_sec) s, typeof(tv_usec) u) { tv_sec = s; tv_usec = u; }
+    myTimeval(time_t s, long u) { set(s,u); }
+    void set(time_t s, long u) { tv_sec = s; tv_usec = u; }
+    const myTimeval& operator=(const timeval &rhs) {
+        tv_sec = rhs.tv_sec;
+        tv_usec = rhs.tv_usec;
+        return *this;
+    }
+    const myTimeval& operator=(const timespec &rhs) {
+        tv_sec = rhs.tv_sec;
+        tv_usec = rhs.tv_nsec / 1000;
+        return *this;
+    }
     const myTimeval& operator-=(const myTimeval &rhs) {
         bool borrow = false;
         if (rhs.tv_usec > tv_usec)
@@ -46,8 +56,9 @@ struct myTimeval : public timeval
     const bool operator!=(const myTimeval &other) {
         return !operator==(other);
     }
-    void getNow(void) {
+    const myTimeval &getNow(void) {
         gettimeofday(this, NULL);
+        return *this;
     }
     const std::string Format(const char *format = NULL) {
         if (format == NULL)
@@ -64,6 +75,12 @@ struct myTimeval : public timeval
     }
     uint32_t msecs(void) {
         return (tv_sec * 1000) + (tv_usec / 1000);
+    }
+    uint64_t usecs(void) {
+        return ((uint64_t)tv_sec * 1000000) + tv_usec;
+    }
+    uint64_t nsecs(void) {
+        return ((uint64_t)tv_sec * 1000000000) + (tv_usec * 1000);
     }
     timeval *operator()(void) { return this; }
 };
@@ -107,5 +124,139 @@ static inline bool operator>(const myTimeval &lhs, const myTimeval &other) {
       return false;
    return lhs.tv_usec > other.tv_usec;
 }
+static inline bool operator<(const myTimeval &lhs, const myTimeval &other) {
+   if (lhs.tv_sec < other.tv_sec) 
+      return true;
+   if (lhs.tv_sec > other.tv_sec) 
+      return false;
+   return lhs.tv_usec < other.tv_usec;
+}
+
+struct myTimespec : public timespec
+{
+    myTimespec(void) { tv_sec = 0; tv_nsec = 0; }
+    myTimespec(time_t s, long n) { set(s,n); }
+    void set(time_t s, long n) { tv_sec = s; tv_nsec = n; }
+    const myTimespec &operator=(const myTimespec &rhs) {
+        tv_sec = rhs.tv_sec;
+        tv_nsec = rhs.tv_nsec;
+        return *this;
+    }
+    const myTimespec &operator=(const timeval &rhs) {
+        tv_sec = rhs.tv_sec;
+        tv_nsec = rhs.tv_usec * 1000;
+        return *this;
+    }
+    const myTimespec &operator-=(const myTimespec &rhs) {
+        bool borrow = false;
+        if (rhs.tv_nsec > tv_nsec)
+            borrow = true;
+        tv_sec -= rhs.tv_sec;
+        tv_nsec -= rhs.tv_nsec;
+        if (borrow)
+        {
+            tv_sec -= 1;
+            tv_nsec += 1000000000;
+        }
+        return *this;
+    }
+    const myTimespec &operator+=(const myTimespec &rhs) {
+        tv_sec += rhs.tv_sec;
+        tv_nsec += rhs.tv_nsec;
+        if (tv_nsec > 1000000000)
+        {
+            tv_nsec -= 1000000000;
+            tv_sec += 1;
+        }
+        return *this;
+    }
+    const bool operator==(const myTimespec &rhs) {
+        return (tv_sec == rhs.tv_sec) && (tv_nsec == rhs.tv_nsec);
+    }
+    const bool operator!=(const myTimespec &rhs) {
+        return !operator==(rhs);
+    }
+    const myTimespec &getNow(clockid_t clk_id = CLOCK_REALTIME) {
+        clock_gettime(clk_id, this);
+        return *this;
+    }
+    const myTimespec &getMonotonic(void) {
+        return getNow(CLOCK_MONOTONIC);
+    }
+    const std::string Format(const char *format = NULL) {
+        if (format == NULL)
+            format = "%Y-%m-%d %H:%M:%S";
+        time_t seconds = tv_sec;
+        struct tm t;
+        localtime_r(&seconds, &t);
+        char ymdhms[128], ns[12];
+        strftime(ymdhms,sizeof(ymdhms),format,&t);
+        ymdhms[sizeof(ymdhms)-1] = 0;
+        snprintf(ns,sizeof(ns),"%09ld", tv_nsec);
+        ns[sizeof(ns)-1] = 0;
+        return std::string(ymdhms) + "." + ns;
+    }
+    uint32_t msecs(void) {
+        return (tv_sec * 1000) + (tv_nsec / 1000000);
+    }
+    uint64_t usecs(void) {
+        return ((uint64_t)tv_sec * 1000000) + (tv_nsec / 1000);
+    }
+    uint64_t nsecs(void) {
+        return ((uint64_t)tv_sec * 1000000000) + tv_nsec;
+    }
+};
+static inline std::ostream& operator<<(std::ostream& ostr,
+                                       const myTimespec &rhs)
+{
+    ostr << "myTimespec(" << rhs.tv_sec << "," << rhs.tv_nsec << ")";
+    return ostr;
+}
+static inline myTimespec operator-(const myTimespec &lhs,
+                                   const myTimespec &rhs) {
+   bool borrow = false;
+   myTimespec tmp;
+   tmp.tv_sec = lhs.tv_sec;
+   tmp.tv_nsec = lhs.tv_nsec;
+   if (rhs.tv_nsec > lhs.tv_nsec)
+      borrow = true;
+   tmp.tv_sec -= rhs.tv_sec;
+   tmp.tv_nsec -= rhs.tv_nsec;
+   if (borrow)
+   {
+      tmp.tv_sec -= 1;
+      tmp.tv_nsec += 1000000000;
+   }
+   return tmp;
+}
+static inline myTimespec operator+(const myTimespec &lhs,
+                                   const myTimespec &rhs) {
+   myTimespec tmp;
+   tmp.tv_sec = lhs.tv_sec + rhs.tv_sec;
+   tmp.tv_nsec = lhs.tv_nsec + rhs.tv_nsec;
+   if (tmp.tv_nsec > 1000000000)
+   {
+      tmp.tv_nsec -= 1000000000;
+      tmp.tv_sec += 1;
+   }
+   return tmp;
+}
+static inline bool operator>(const myTimespec &lhs,
+                             const myTimespec &other) {
+   if (lhs.tv_sec > other.tv_sec) 
+      return true;
+   if (lhs.tv_sec < other.tv_sec) 
+      return false;
+   return lhs.tv_nsec > other.tv_nsec;
+}
+static inline bool operator<(const myTimespec &lhs,
+                             const myTimespec &other) {
+   if (lhs.tv_sec < other.tv_sec) 
+      return true;
+   if (lhs.tv_sec > other.tv_sec) 
+      return false;
+   return lhs.tv_nsec < other.tv_nsec;
+}
+
 
 #endif /* __MYTIMEVAL_H__ */
