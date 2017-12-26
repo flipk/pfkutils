4da6f020ef22f7aa5f9f2a881e3e17d17de412ab
Phillip F Knaack <pknaack1@gmail.com>
2017-12-26 14:51:29 -0600

    working libprotossl version 2!

--- libprotossl/libprotossl2.h
+++ libprotossl/libprotossl2.h
@@ -14,9 +14,12 @@
 #include "posix_fe.h"
 #include "dll3.h"
 
-// namespace ProtoSSL {
+namespace ProtoSSL {
 
 typedef google::protobuf::Message MESSAGE;
+class ProtoSSLMsgs;
+
+/*************************** ProtoSSLCertParams ***************************/
 
 struct ProtoSSLCertParams
 {
@@ -24,49 +27,43 @@ struct ProtoSSLCertParams
     const std::string &myCert;
     const std::string &myKey;
     const std::string &myKeyPassword;
-    const std::string &otherCommonName;
     ProtoSSLCertParams(const std::string &_caCert, // file:/...
                        const std::string &_myCert, // file:/...
                        const std::string &_myKey,  // file:/...
-                       const std::string &_myKeyPassword,
-                       const std::string &_otherCommonName);
+                       const std::string &_myKeyPassword);
     ~ProtoSSLCertParams(void);
 };
 
-class ProtoSSLMsgs;
+/*************************** ProtoSSLConnClient ***************************/
+
 class ProtoSSLConnClient;
 class ProtoSSLConnClientHash;
-class ProtoSSLConnServer;
-class ProtoSSLConnServerHash;
-
 typedef DLL3::List<ProtoSSLConnClient, 1/*uniqueID*/,
-                   false/*lockWarn*/,false/*validate*/>  ClientList_t;
+                   false/*lockWarn*/,true/*validate*/>  ClientList_t;
 typedef DLL3::Hash<ProtoSSLConnClient, int/*fd*/,
                    ProtoSSLConnClientHash, 2/*uniqueID*/,
-                   false/*lockWarn*/,false/*validate*/>  ClientHash_t;
-
-typedef DLL3::List<ProtoSSLConnServer,/*uniqueID*/3,
-                   false/*lockWarn*/,false/*validate*/>  ServerList_t;
-typedef DLL3::Hash<ProtoSSLConnServer, int/*fd*/,
-                   ProtoSSLConnServerHash, 4/*uniqueID*/,
-                   false/*lockWarn*/,false/*validate*/>  ServerHash_t;
+                   false/*lockWarn*/,true/*validate*/>  ClientHash_t;
 
 class ProtoSSLConnClient : public ClientList_t::Links,
                            public ClientHash_t::Links
 {
     friend class ProtoSSLMsgs;
     friend class ProtoSSLConnClientHash;
+    friend class ProtoSSLConnServer;
+    bool _ok;
+    bool send_close_notify;
     mbedtls_net_context netctx;
     mbedtls_ssl_context sslctx;
-    // private so only ProtoSSLMsgs can make it.
-    int fd;
     ProtoSSLMsgs * msgs;
-    ProtoSSLConnClient(ProtoSSLMsgs * _msgs);
+    // private so only ProtoSSLMsgs can make it.
+    ProtoSSLConnClient(ProtoSSLMsgs * _msgs, mbedtls_net_context new_netctx);
+    ProtoSSLConnClient(ProtoSSLMsgs * _msgs,
+                       const std::string &remoteHost, int remotePort);
+    bool init_common(void); // returns ok
 public:
     virtual ~ProtoSSLConnClient(void);
-    int get_fd(void) const { return fd; };
+    int get_fd(void) const { return netctx.fd; };
     enum read_return_t {
-        GOT_CONNECT,
         GOT_DISCONNECT,
         READ_MORE,
         GOT_MESSAGE
@@ -74,47 +71,54 @@ public:
     read_return_t handle_read(MESSAGE &msg);
     // returns true if ok, false if not
     bool send_message(const MESSAGE &msg);
+    bool ok(void) const { return _ok; }
 };
 class ProtoSSLConnClientHash {
 public:
-    static uint32_t obj2hash  (const ProtoSSLConnClient &obj) {
-        return obj.fd;
-    }
-    static uint32_t key2hash  (const int fd) {
-        return fd;
-    }
-    static bool     hashMatch (const int fd, const ProtoSSLConnClient &obj) {
-        return fd == obj.fd;
-    }
+    static uint32_t obj2hash  (const ProtoSSLConnClient &obj)
+    { return obj.get_fd(); }
+    static uint32_t key2hash  (const int fd) { return fd; }
+    static bool     hashMatch (const int fd, const ProtoSSLConnClient &obj)
+    { return fd == obj.get_fd(); }
 };
+
+/*************************** ProtoSSLConnServer ***************************/
+
+class ProtoSSLConnServer;
+class ProtoSSLConnServerHash;
+typedef DLL3::List<ProtoSSLConnServer,/*uniqueID*/3,
+                   false/*lockWarn*/,true/*validate*/>  ServerList_t;
+typedef DLL3::Hash<ProtoSSLConnServer, int/*fd*/,
+                   ProtoSSLConnServerHash, 4/*uniqueID*/,
+                   false/*lockWarn*/,true/*validate*/>  ServerHash_t;
 
 class ProtoSSLConnServer : public ServerList_t::Links,
                            public ServerHash_t::Links
 {
     friend class ProtoSSLMsgs;
     friend class ProtoSSLConnServerHash;
-    // private so only ProtoSSLMsgs can make it.
-    int fd;
+    bool _ok;
+    mbedtls_net_context netctx;
     ProtoSSLMsgs * msgs;
-    ProtoSSLConnServer(ProtoSSLMsgs * _msgs);
+    // private so only ProtoSSLMsgs can make it.
+    ProtoSSLConnServer(ProtoSSLMsgs * _msgs, int listeningPort);
 public:
     virtual ~ProtoSSLConnServer(void);
-    int get_fd(void) const { return fd; };
+    int get_fd(void) const { return netctx.fd; };
     // returns NULL if accept failed for some reason
     ProtoSSLConnClient * handle_accept(void);
+    bool ok(void) const { return _ok; }
 };
 class ProtoSSLConnServerHash {
 public:
-    static uint32_t obj2hash  (const ProtoSSLConnServer &obj) {
-        return obj.fd;
-    }
-    static uint32_t key2hash  (const int fd) {
-        return fd;
-    }
-    static bool     hashMatch (const int fd, const ProtoSSLConnServer &obj) {
-        return fd == obj.fd;
-    }
+    static uint32_t obj2hash  (const ProtoSSLConnServer &obj)
+    { return obj.get_fd(); }
+    static uint32_t key2hash  (const int fd) { return fd; }
+    static bool     hashMatch (const int fd, const ProtoSSLConnServer &obj)
+    { return fd == obj.get_fd(); }
 };
+
+/*************************** ProtoSSLMsgs ***************************/
 
 class ProtoSSLMsgs
 {
@@ -125,6 +129,7 @@ class ProtoSSLMsgs
     mbedtls_x509_crt         cacert, mycert;
     mbedtls_pk_context       mykey;
     mbedtls_ssl_config       sslcfg;
+    bool                     nonBlockingMode;
     bool                     debugFlag;
     ClientList_t             clientList;
     ClientHash_t             clientHash;
@@ -135,7 +140,7 @@ class ProtoSSLMsgs
     void   registerClient(ProtoSSLConnClient * clnt);
     void unregisterClient(ProtoSSLConnClient * clnt);
 public:
-    ProtoSSLMsgs(bool _debugFlag = false);
+    ProtoSSLMsgs(bool _nonBlockingMode, bool _debugFlag = false );
     ~ProtoSSLMsgs(void);
     // returns true if it could load, false if error
     bool loadCertificates(const ProtoSSLCertParams &params);
@@ -144,6 +149,6 @@ public:
                                      int remotePort);
 };
 
-// }; // namespace ProtoSSL
+}; // namespace ProtoSSL
 
 #endif /* __LIBPROTOSSL2_H__ */
---
+++ libprotossl/test2.cc
@@ -0,0 +1,292 @@
+
+#include "libprotossl2.h"
+#include TEST_PROTO_HDR
+
+using namespace PFK::Test;
+
+class test_program
+{
+    ServerToClient stc;
+    ClientToServer cts;
+    bool connected;
+    ProtoSSL::ProtoSSLMsgs  msgs;
+    ProtoSSL::ProtoSSLConnClient * client;
+    ProtoSSL::ProtoSSLConnServer * server;
+public:
+    test_program(void) : msgs(/*nonblock*/true, /*debugFlag*/false)
+    {
+        connected = false;
+        client = NULL;
+        server = NULL;
+    }
+    int main(int argc, char ** argv)
+    {
+        std::string cert_ca        = "file:keys/Root-CA.crt";
+
+        std::string cert_server    = "file:keys/Server-Cert.crt";
+        std::string key_server     = "file:keys/Server-Cert-encrypted.key";
+        std::string key_pwd_server = "0KZ7QMalU75s0IXoWnhm3BXEtswirfwrXwwNiF6c";
+
+        std::string cert_client    = "file:keys/Client-Cert.crt";
+        std::string key_client     = "file:keys/Client-Cert-encrypted.key";
+        std::string key_pwd_client = "IgiLNFWx3fTMioJycI8qXCep8j091yfHOwsBbo6f";
+
+        if (argc < 2)
+        {
+            return 1;
+        }
+        std::string argv1(argv[1]);
+        if (argv1 == "s")
+        {
+            ProtoSSL::ProtoSSLCertParams  certs(cert_ca,
+                                                cert_server,
+                                                key_server,
+                                                key_pwd_server);
+
+            if (msgs.loadCertificates(certs) == false)
+                return 1;
+
+            server = msgs.startServer(2005);
+            if (server == NULL)
+            {
+                printf("failure to chooch making server\n");
+                return 1;
+            }
+        }
+        else if (argv1 == "c")
+        {
+            if (argc != 3)
+            {
+                printf("specify ip address of server\n");
+                return 2;
+            }
+
+            ProtoSSL::ProtoSSLCertParams  certs(cert_ca,
+                                                cert_client,
+                                                key_client,
+                                                key_pwd_client);
+
+            if (msgs.loadCertificates(certs) == false)
+                return 1;
+
+            client = msgs.startClient(argv[2], 2005);
+            if (client == NULL)
+            {
+                printf("failure to chooch making client\n");
+                return 1;
+            }
+            connected = true;
+            send_server_protoversion();
+        }
+        else
+        {
+            return 2;
+        }
+
+        bool done = false;
+        while (!done)
+        {
+            pxfe_select sel;
+
+            if (server)
+                sel.rfds.set(server->get_fd());
+            if (client)
+                sel.rfds.set(client->get_fd());
+            if (connected)
+                sel.rfds.set(0);
+            sel.tv.set(1,0);
+
+            sel.select();
+
+            if (server && sel.rfds.is_set(server->get_fd()))
+            {
+                ProtoSSL::ProtoSSLConnClient * newclient =
+                    server->handle_accept();
+
+                if (newclient)
+                {
+                    if (client)
+                    {
+                        printf("rejecting new connection\n");
+                        delete newclient;
+                    }
+                    else
+                    {
+                        printf("got connection!\n");
+                        client = newclient;
+                        connected = true;
+                        send_client_protoversion();
+                    }
+                }
+            }
+
+            if (client && sel.rfds.is_set(client->get_fd()))
+            {
+                if (server)
+                {
+                    switch (client->handle_read(cts))
+                    {
+                    case ProtoSSL::ProtoSSLConnClient::GOT_DISCONNECT:
+                        printf("got disconnect\n");
+                        done = true;
+                        break;
+                    case ProtoSSL::ProtoSSLConnClient::READ_MORE:
+                        // nothing to do here
+                        break;
+                    case ProtoSSL::ProtoSSLConnClient::GOT_MESSAGE:
+                        done = handle_client_msg();
+                        break;
+                    }
+                }
+                else
+                {
+                    switch (client->handle_read(stc))
+                    {
+                    case ProtoSSL::ProtoSSLConnClient::GOT_DISCONNECT:
+                        printf("got disconnect\n");
+                        done = true;
+                        break;
+                    case ProtoSSL::ProtoSSLConnClient::READ_MORE:
+                        // nothing to do here
+                        break;
+                    case ProtoSSL::ProtoSSLConnClient::GOT_MESSAGE:
+                        done = handle_server_msg();
+                        break;
+                    }
+                }
+            }
+
+            if (sel.rfds.is_set(0))
+            {
+                pxfe_string  buffer;
+                buffer.resize(100);
+                int cc = ::read(0, buffer.vptr(), buffer.length());
+                if (cc > 0)
+                {
+                    buffer.resize(cc);
+                    if (server)
+                    {
+                        ServerToClient stc;
+                        stc.set_type(STC_DATA);
+                        stc.mutable_data()->set_data(buffer);
+                        client->send_message(stc);
+                        stc.Clear();
+                    }
+                    else
+                    {
+                        ClientToServer cts;
+                        cts.set_type(CTS_DATA);
+                        cts.mutable_data()->set_data(buffer);
+                        client->send_message(cts);
+                        cts.Clear();
+                    }
+                }
+                else
+                    done = true;
+            }
+        }
+
+        return 0;
+    }
+private:
+    void send_server_protoversion(void)
+    {
+        cts.Clear();
+        cts.set_type(CTS_PROTO_VERSION);
+        cts.mutable_proto_version()->set_app_name("LIBPROTOSSL_TEST2");
+        cts.mutable_proto_version()->set_version(PROTOCOL_VERSION_3);
+        client->send_message(cts);
+        cts.Clear();
+    }
+
+    void send_client_protoversion(void)
+    {
+        ServerToClient stc;
+        stc.set_type(STC_PROTO_VERSION);
+        stc.mutable_proto_version()->set_app_name("LIBPROTOSSL_TEST2");
+        stc.mutable_proto_version()->set_version(PROTOCOL_VERSION_3);
+        client->send_message(stc);
+        stc.Clear();
+    }
+
+    void timeval_to_PingInfo(PingInfo &pi, const pxfe_timeval &tv)
+    {
+        pi.set_time_seconds (tv.tv_sec );
+        pi.set_time_useconds(tv.tv_usec);
+    }
+
+    void PingInfo_to_timeval(pxfe_timeval &tv, const PingInfo &pi)
+    {
+        tv.tv_sec  = pi.time_seconds ();
+        tv.tv_usec = pi.time_useconds();
+    }
+
+    bool handle_server_msg(void)
+    {
+        printf("got msg from server: %s\n", stc.DebugString().c_str());
+        switch (stc.type())
+        {
+        case STC_PROTO_VERSION:
+            send_server_ping(1);
+            break;
+        case STC_PING_ACK:
+            handle_ping_ack();
+            break;
+        }
+        return false;
+    }
+
+    bool handle_client_msg(void)
+    {
+        printf("got msg from client: %s\n", cts.DebugString().c_str());
+        switch (cts.type())
+        {
+        case CTS_PING:
+            send_client_ping_ack();
+            break;
+        }
+        return false;
+    }
+
+    void send_server_ping(int seq)
+    {
+        pxfe_timeval now;
+        cts.set_type(CTS_PING);
+        cts.mutable_ping()->set_seq(seq);
+        now.getNow();
+        timeval_to_PingInfo(*cts.mutable_ping(), now);
+        client->send_message(cts);
+        cts.Clear();
+    }
+
+    void send_client_ping_ack(void)
+    {
+        stc.set_type(STC_PING_ACK);
+        stc.mutable_ping()->CopyFrom(*cts.mutable_ping());
+        client->send_message(stc);
+        stc.Clear();
+    }
+
+    void handle_ping_ack(void)
+    {
+        pxfe_timeval ts, now, diff;
+        uint32_t seq = stc.mutable_ping()->seq();
+        now.getNow();
+        PingInfo_to_timeval(ts, *stc.mutable_ping());
+        diff = now - ts;
+        printf("client got PING_ACK seq %d delay %u.%06u\n",
+               seq,
+               (unsigned int) diff.tv_sec,
+               (unsigned int) diff.tv_usec);
+        if (seq < 3)
+            send_server_ping(seq+1);
+    }
+
+};
+
+
+int main(int argc, char ** argv)
+{
+    test_program tp;
+    return tp.main(argc, argv);
+}
