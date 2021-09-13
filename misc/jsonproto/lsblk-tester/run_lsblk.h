540c7a1a171dcc33ecd4899d3dcab8b45aa23673
Phillip F Knaack <pknaack1@gmail.com>
2021-09-12 20:21:23 -0500

    jsonproto test code (using lsblk)

---
+++ misc/jsonproto/lsblk-tester/run_lsblk.h
@@ -0,0 +1,9 @@
+
+#ifndef __RUN_LSBLK_H__
+#define __RUN_LSBLK_H__ 1
+
+#include LSBLK_OUTPUT_PROTO_HDR
+
+bool run_lsblk(pfk::diskmanager::lsblk::BlockDevices_m &devs);
+
+#endif /* __RUN_LSBLK_H__ */
