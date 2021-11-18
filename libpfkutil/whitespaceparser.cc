4ec100d8ab10320ba3acd6775a94391ab75f8515
Phillip F Knaack <pknaack1@gmail.com>
2021-11-17 21:42:06 -0600

    new whitespaceparser parseString is more better

--- libpfkutil/whitespaceparser.cc
+++ libpfkutil/whitespaceparser.cc
@@ -1,10 +1,31 @@
 
-vector<string> parseString(string input, char delimiter)
+#include <string>
+#include <vector>
+#include <sstream>
+
+void parseString(std::vector<std::string> &tokens,
+                 std::string &input, char delimiter)
 {
-    stringstream parser(input);
-    string token;
-    vector<string> tokens;
-    while (getline(parser,token,delimiter))
-        tokens.push_back(token);
-    return tokens;
+    std::istringstream parser(input);
+    std::string token;
+    tokens.clear();
+    while (std::getline(parser,token,delimiter))
+        if (token.length() > 0)
+            tokens.push_back(token);
+}
+
+int main(int argc, char ** argv)
+{
+    if (argc == 2)
+    {
+        std::string in(argv[1]);
+        std::vector<std::string> tokens;
+
+        parseString(tokens, in, ' ');
+
+        for (auto t : tokens)
+            printf("token : %s\n", t.c_str());
+    }
+
+    return 0;
 }
