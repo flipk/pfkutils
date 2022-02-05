06b6278d41e5ce4e88728e41b5bf820896b89951
Phillip F Knaack <pknaack1@gmail.com>
2022-02-05 13:26:51 -0600

    simpleWsTestServer should demo how to use select

--- libWebAppServer/simpleWsTestServer.cc
+++ libWebAppServer/simpleWsTestServer.cc
@@ -5,6 +5,8 @@
 #endif
 #include <map>
 #include <pthread.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 #define DEBUG false
 
@@ -50,16 +52,36 @@ void *connection_thread(void*arg)
     conns[pos] = nc;
     pthread_mutex_unlock(&mutex);
 
+    int fd = nc->get_fd();
+    fcntl( fd, F_SETFL,
+           fcntl( fd, F_GETFL, 0 ) | O_NONBLOCK );
+    bool doselect = false;
+
     bool done = false;
     while (!done)
     {
-        SimpleWebSocket::WebSocketRet ret;
-        ret = nc->handle_read(msg);
+        if (doselect) {
+            bool doservice = false;
+            while (!doservice) {
+                fd_set rfds;
+                struct timeval tv;
+                FD_ZERO(&rfds);
+                FD_SET(fd, &rfds);
+                tv.tv_sec = 1; // 1 second polling
+                tv.tv_usec = 0;
+                (void) select(fd+1, &rfds, NULL, NULL, &tv);
+                if (FD_ISSET(fd, &rfds))
+                    doservice = true;
+            }
+        }
+
+        SimpleWebSocket::WebSocketRet ret = nc->handle_read(msg);
         switch (ret)
         {
         case SimpleWebSocket::WEBSOCKET_CONNECTED:
         {
             std::string path;
+            doselect = false;
             nc->get_path(path);
             printf("WebSocket connected! path = '%s'\n",
                    path.c_str());
@@ -72,9 +94,11 @@ void *connection_thread(void*arg)
         }
         case SimpleWebSocket::WEBSOCKET_NO_MESSAGE:
             // go round again
+            doselect = true;
             break;
 
         case SimpleWebSocket::WEBSOCKET_MESSAGE:
+            doselect = false;
             switch (msg.type())
             {
             case proxyTcp::PMT_PROTOVERSION:
