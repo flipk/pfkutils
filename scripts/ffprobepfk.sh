fc18ff2b4af5fc063c50537b5c99cc392bbc7e96
Phillip F Knaack <pknaack1@gmail.com>
2022-06-15 20:27:31 -0500

    ffprobepfk needs to handle webm, video-only, and audio-only files better

--- scripts/ffprobepfk.sh
+++ scripts/ffprobepfk.sh
@@ -25,19 +25,26 @@ for f in $* ; do
             FILENAME="$f" FILESIZE="$1" awk '
 
 BEGIN {
+   vcodec = "NONE";
    acodec = "NONE";
 }
 
 $1=="Duration:" {
    gsub(/\.[0-9][0-9],/, "", $2);
    duration=$2;
+   if ($5 == "bitrate:") {
+      vkbps = $6;
+   }
 }
 
 /Stream .* Video:/ {
    vcodec      = gensub(/^.*Video: ([a-zA-Z0-9]+).*$/, "\\1", "g", $0);
    dimensionsX = gensub(/^.* ([0-9]*)x([0-9]*).*$/, "\\1", "g", $0);
    dimensionsY = gensub(/^.* ([0-9]*)x([0-9]*).*$/, "\\2", "g", $0);
-   vkbps       = gensub(/^.* ([0-9]*) kb.s.*$/, "\\1", "g", $0);
+   vkbps_tmp   = gensub(/^.* ([0-9]*) kb.s.*$/, "\\1", "g", $0);
+   if (vkbps_tmp != $0) {
+      vkbps = vkbps_tmp;
+   }
    fps         = gensub(/^.* ([0-9\.]*) fps.*$/, "\\1", "g", $0);
 }
 
@@ -48,13 +55,23 @@ $1=="Duration:" {
 }
 
 END {
-   printf("%s %4s %4d %4d %5d %6.2f %4s %5u %4u %10u %s\n",
-          duration, vcodec, dimensionsX, dimensionsY, vkbps, fps,
-          acodec, samplerate, akbps,
-          ENVIRON["FILESIZE"], ENVIRON["FILENAME"] );
-}
+   printf("%s ", duration);
 
+   if (vcodec != "NONE") {
+      printf("%4s %4d %4d %5d %6.2f ",
+             vcodec, dimensionsX, dimensionsY, vkbps, fps);
+   } else {
+      printf("                            ");
+   }
 
+   if (acodec != "NONE") {
+      printf("%4s %5u %4u ", acodec, samplerate, akbps);
+   } else {
+      printf("                ");
+   }
+
+   printf("%10u %s\n", ENVIRON["FILESIZE"], ENVIRON["FILENAME"] );
+} 
 
 ' $out
 
