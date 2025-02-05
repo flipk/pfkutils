e5fd68d59b89688acf0bc60c796971016e0c6cc0
Phillip F Knaack <pknaack1@gmail.com>
2025-02-04 21:11:06 -0600

    fixed wichmann-hill, oops

--- misc/wichmann_hill_as183.cc
+++ misc/wichmann_hill_as183.cc
@@ -3,12 +3,17 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <time.h>
-#include <iostream>
+#include <stdio.h>
+#include <math.h>
 
 using namespace std;
 
 // Wichmann-Hill  algorithm AS183
 // or https://en.wikipedia.org/wiki/Wichmann-Hill
+
+// produces uniformly distributed values between 0.0 and 1.0.
+// The overall period is the least common multiple of these:
+//   30268 × 30306 × 30322 / 4 = 6,953,607,871,644
 
 class wichmann_hill_as183
 {
@@ -21,11 +26,13 @@ public:
         s2 = random() % 30000;
         s3 = random() % 30000;
     }
-    int operator() () {
+    float operator() () {
         s1 = (s1 * 171) % 30269;
         s2 = (s2 * 172) % 30307;
         s3 = (s3 * 170) % 30323;
-        return (s1 % 30269 + s2 % 30307 + s3 % 30323);
+        return fmod(((float)s1 / 30269.0 +
+                     (float)s2 / 30307.0 +
+                     (float)s3 / 30323.0), 1.0);
     }
 };
 
@@ -35,8 +42,8 @@ int main()
 
     wichmann_hill_as183   r;
 
-    for (int ind = 0; ind < 1000000; ind++)
-        cout << r() << endl;
+    for (int ind = 0; ind < 10000; ind++)
+        printf("%f\n", r());
 
     return 0;
 }
